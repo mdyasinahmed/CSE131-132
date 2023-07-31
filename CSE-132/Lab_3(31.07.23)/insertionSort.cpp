3bed3999c7d7cb972eacfbd4c8a2c3b3c3a477a1
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-31 14:07:12 +0600

    add in insertion sort code with cpp

---
+++ CSE-132/Lab_3(31.07.23)/insertionSort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size) {
+  for(int i=1; i<size; i++) {
+    int key = arr[i];
+    int j = i - 1;
+
+    while(key < arr[j] && j >= 0){
+      arr[j + 1] = arr[j];
+      --j;
+    }
+    arr[j + 1] = key;
+  }
+
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+
+    // scan array
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    
+    insertionSort(a,n);
+
+
+
+}
+
