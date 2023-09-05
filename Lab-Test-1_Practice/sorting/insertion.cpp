267ba7049561bc78a95694d65b72b2ad5d4ae0a4
mdyasinahmed <yasinahmed7875@gmail.com>
2023-09-05 22:55:32 +0600

    update insertion sort code

--- Lab-Test-1_Practice/sorting/insertion.cpp
+++ Lab-Test-1_Practice/sorting/insertion.cpp
@@ -15,13 +15,20 @@ void insertionSort(int arr[], int n) {
 }
 
 void print(int arr[], int n) {
-    for(int i=0; i<n; i++) { cout << arr[i] <<" "; }
+    for(int i=0; i<n; i++) { 
+        cout << arr[i] <<" ";
+    }
 }
 
 int main() {
-    int n; cin >> n; int arr[n];
+    int n; 
+    cin >> n; 
+    int arr[n];
 
-    for(int i=0; i<n; i++) { cin >> arr[i]; }
+    for(int i=0; i<n; i++) { 
+        cin >> arr[i]; 
+    }
+
     insertionSort(arr, n);
     print(arr, n);
     
