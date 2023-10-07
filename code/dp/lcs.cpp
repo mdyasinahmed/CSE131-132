fe55114ccb437a6b858e7c03cc69dd0c35d91112
mdyasinahmed <yasinahmed7875@gmail.com>
2023-10-07 13:10:20 +0600

    add lcs code with c++

--- code/dp/lcs.cpp
+++ code/dp/lcs.cpp
@@ -1,49 +1,60 @@
 // The longest common subsequence in C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void lcsAlgo(char *S1, char *S2, int m, int n) {
-  int LCS_table[m + 1][n + 1];
+void lcsAlgo(char *S1, char *S2, int m, int n)
+{
+    int LCS_table[m + 1][n + 1];
 
-
-  // Building the mtrix in bottom-up way
-  for (int i = 0; i <= m; i++) {
-    for (int j = 0; j <= n; j++) {
-      if (i == 0 || j == 0) LCS_table[i][j] = 0;
-      else if (S1[i - 1] == S2[j - 1]) LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
-      else LCS_table[i][j] = max(LCS_table[i - 1][j], LCS_table[i][j - 1]);
+    // Building the matrix in bottom-up way
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+                LCS_table[i][j] = 0;
+            else if (S1[i - 1] == S2[j - 1])
+                LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
+            else
+                LCS_table[i][j] = max(LCS_table[i - 1][j], LCS_table[i][j - 1]);
+            cout << LCS_table[i][j] << " ";
+        }
+        cout << endl;
     }
-  }
+    cout << m << endl;
+    cout << n << endl;
+    int index = LCS_table[m][n];
+    cout << index << endl;
+    char lcsAlgo[index + 1];
+    
+    lcsAlgo[index] = '\0';
 
-  int index = LCS_table[m][n];
-  char lcsAlgo[index + 1];
-  lcsAlgo[index] = '\0';
-
-  int i = m, j = n;
-  while(i > 0 && j > 0) {
-    if(S1[i - 1] == S2[j - 1]) {
-      lcsAlgo[index - 1] = S1[i - 1];
-      i--;
-      j--;
-      index--;
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+        if(S1[i-1] == S2[j-1]) {
+            lcsAlgo[index-1] = S1[i-1];
+            i--;
+            j--;
+            index--;
+        }
+        else if (LCS_table[i - 1][j] > LCS_table[i][j - 1])
+            i--;
+        else
+            j--;
     }
-    else if (LCS_table[i - 1][j] > LCS_table[i][j - 1])
-      i--;
-    else
-      j--;
-  }
-  
-  // Printing the sub sequences
-  //cout << "S1 : " << S1 << "\nS2 : " << S2 << "\nLCS: " << lcsAlgo << "\n";
-  cout << "LCS: " << lcsAlgo << "\n";
+
+    // Printing the sub sequences
+    cout << "S1 : " << S1 << "\nS2 : " << S2 << "\nLCS: " << lcsAlgo << "\n";
+    // cout << "LCS: " << lcsAlgo << "\n";
 }
 
-int main() {
-  char S1[] = "ACADB";
-  char S2[] = "CBDA";
-  int m = strlen(S1);
-  int n = strlen(S2);
+int main()
+{
+    char S1[] = "ACADB";
+    char S2[] = "CBDA";
+    int m = strlen(S1);
+    int n = strlen(S2);
 
-  lcsAlgo(S1, S2, m, n);
+    lcsAlgo(S1, S2, m, n);
 }
