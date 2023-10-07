50f68a8e42b2877ab6e6a85ae071e7a05219a2a2
mdyasinahmed <yasinahmed7875@gmail.com>
2023-10-07 13:10:46 +0600

    add coin change: maxWays code with c++

--- code/dp/coins/maxWays.cpp
+++ code/dp/coins/maxWays.cpp
@@ -3,26 +3,30 @@
 
 using namespace std;
 
-int countWays(vector<int>& coins, int amount) {
-    vector<int> dp(amount + 1, 0);
-    dp[0] = 1;
+int countWays(vector <int> &coins, int amount) {
+    vector <int> table(amount+1, 0);
+    table[0] = 1;
+    cout << table[0] << endl;
 
-    for (int coin : coins) {
-        for (int i = coin; i <= amount; i++) {
-            dp[i] += dp[i - coin];
+    for(int coin : coins) {
+        for(int i = coin; i <= amount; i++) {
+            table[i] = table[i] + table[i-coin];
+            cout << table[i] << " ";
         }
+        cout << endl;
     }
 
-    return dp[amount];
+    return table[amount];
 }
 
+
 int main() {
-    vector<int> coins = {1, 2, 5};
-    int amount = 5;
+    vector<int> coins = {1, 3, 5};
+    int amount = 8;
 
     int ways = countWays(coins, amount);
 
-    cout << "Number of ways to make " << amount << " with given coins is: " << ways << endl;
+    cout << "\nNumber of ways to make " << amount << " with given coins is: " << ways << endl;
 
     return 0;
 }
