cdab3ddee9eb4caa15bf993d18b8c5256e623288
mdyasinahmed <yasinahmed7875@gmail.com>
2023-10-15 23:36:44 +0600

    update 0/1 knapsack code

--- code/dp/knapsack/0-1_knapsack.cpp
+++ code/dp/knapsack/0-1_knapsack.cpp
@@ -1,41 +1,67 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
+// Structure to represent an item
 struct Item {
     int weight;
-    int value;
+    int profit;
 };
 
-int knapsack(vector<Item>& items, int capacity) {
+// Function to solve the 0/1 Knapsack problem
+pair<int, vector<int>> knapsack(vector<Item>& items, int capacity) {
     int n = items.size();
-    vector<vector<int>> beg(n + 1, vector<int>(capacity + 1, 0));
+    vector<vector<int>> table(n+1, vector<int>(capacity+1, 0));
 
-    for(int i = 1; i <= n; i++) {
-        for(int w = 0; w <= capacity; w++) {
-            if(items[i - 1].weight <= w) {
-                beg[i][w] = max(beg[i - 1][w], beg[i - 1][w - items[i - 1].weight] + items[i - 1].value);
+    // Build the table
+    for (int i = 1; i <= n; i++) {
+        for (int w = 0; w <= capacity; w++) {
+            if (items[i-1].weight > w) {
+                table[i][w] = table[i-1][w];
             } else {
-                beg[i][w] = beg[i - 1][w];
+                table[i][w] = max(table[i-1][w], table[i-1][w - items[i-1].weight] + items[i-1].profit);
             }
         }
     }
 
-    return beg[n][capacity];
+    // Find the selected items
+    vector<int> selectedItems;
+    int w = capacity;
+    for (int i = n; i > 0 && w > 0; i--) {
+        if (table[i][w] != table[i-1][w]) {
+            selectedItems.push_back(i-1);
+            w -= items[i-1].weight;
+        }
+    }
+
+    return make_pair(table[n][capacity], selectedItems);
 }
 
-
 int main() {
-    vector<Item> items = {
-        {10, 60},
-        {20, 100},
-        {30, 120}
-    };
+    int capacity, n;
+    cout << "Enter the knapsack capacity: ";
+    cin >> capacity;
 
-    int knapsackCapacity = 50;
+    cout << "Enter the number of items: ";
+    cin >> n;
 
-    int maxValue = knapsack(items, knapsackCapacity);
+    vector<Item> items(n);
 
-    cout << "Maximum value obtained = " << maxValue << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Item " << i + 1 << ", Weight and Profit " << ": ";
+        cin >> items[i].weight >> items[i].profit;
+    }
+
+    pair<int, vector<int>> result = knapsack(items, capacity);
+
+    cout << "Maximum Profit: " << result.first << endl;
+    cout << "Selected Items: ";
+
+    for (int i = result.second.size() - 1; i >= 0; i--) {
+        cout << result.second[i] + 1 << " ";
+    }
+    cout << endl;
 
     return 0;
 }
